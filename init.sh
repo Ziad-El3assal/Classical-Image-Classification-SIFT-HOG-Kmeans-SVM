e2d5f3ac00787a3622ae43c59fd7b63a1872e417
ZiadSENG <ziad.seng@gmail.com>
2024-04-01 13:34:23 +0200

    Updating init file to create env and install libraries

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
+Bash
+python3 -m venv venv
+
+source venv/bin/activate
+
 ./kaggle/kaggle.json
-pip install kaggle
+pip install scikit-learn opencv-python pandas numpy kaggle
 kaggle datasets download -d puneet6060/intel-image-classification
 Expand-Archive intel-image-classification.zip
 rm intel-image-classification.zip
